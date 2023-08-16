f867ce10d903105d3b257f9852650b6063f1e126
ward.zhang@quectel.com <ward.zhang@quectel.com>
2023-08-16 21:36:22 +0800

    增加卡片组件

---
+++ miniprogram/components/card/index.js
@@ -0,0 +1,26 @@
+// components/card/index.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    icon:String,
+    title: String,
+    comment: String,
+    isVideo: Boolean
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
