da4d74aed0d93f6bc2dc3e75031edfae1c773f62
ward.zhang@quectel.com <ward.zhang@quectel.com>
2023-08-17 10:59:32 +0800

    增加卡片 video样式

--- miniprogram/components/card/index.js
+++ miniprogram/components/card/index.js
@@ -7,20 +7,32 @@ Component({
     icon:String,
     title: String,
     comment: String,
-    isVideo: Boolean
+    isVideo: Boolean,
+    videoIcon: String
   },
 
   /**
    * Component initial data
    */
   data: {
-
+    imageHeight: 0,
+    imageWidth: 0
   },
 
   /**
    * Component methods
    */
   methods: {
-
+    imageOnload(event) {
+      console.log(event.detail)
+      const {width, height} = event.detail
+      const screenWidth = wx.getWindowInfo().screenWidth
+      const imageWidth =  screenWidth / 2 - 6
+      const imageHeight = ( height / width )  * imageWidth
+      this.setData({
+        imageHeight: imageHeight,
+        imageWidth: imageWidth
+      })
+    }
   }
 })
